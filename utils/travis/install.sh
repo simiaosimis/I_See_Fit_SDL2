d084619e89a22a81ed1fda5c7a3e0fac0a3355bc
CaioIcy <caio.nardelli@hotmail.com>
2015-01-23 17:32:14 -0200

    Trying fix 'no video device' in Travis

--- utils/travis/install.sh
+++ utils/travis/install.sh
@@ -13,5 +13,5 @@ sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.8 80
 
 ##############################################################################
 # Add your projects necessary "install" commands here
-sudo apt-get install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev graphviz
+sudo apt-get install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev graphviz libglew-dev
 ##############################################################################
