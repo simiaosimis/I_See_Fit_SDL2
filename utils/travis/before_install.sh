e1cfa8144d90f2c13740f55ba3d8819a749d44e2
CaioIcy <caio.nardelli@hotmail.com>
2015-01-24 00:14:55 -0200

    :eight: Updating TravisCI doxygen version

--- utils/travis/before_install.sh
+++ utils/travis/before_install.sh
@@ -9,7 +9,8 @@ sudo add-apt-repository --yes ppa:kubuntu-ppa/backports   # cmake over 2.8.7
 
 ##############################################################################
 # Add your projects necessary "before_install" commands here
-sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshots	# sdl2
+sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshots		  # sdl2
+sudo apt-add-repository --yes ppa:libreoffice/libreoffice-4-2 # doxygen 1.8
 ##############################################################################
 
 ##############################################################################
